d7bf543a5790f61746d7351deec50966c50dd74f
aert <dev.aert@gmail.com>
2014-02-27 20:25:45 +0100

    changing project name.

--- microblog/app.py
+++ microblog/app.py
@@ -6,7 +6,7 @@ __all__ = ["create_app"]
 
 def create_app(app_name):
     app = Flask(app_name)
-    app.config.from_object("tutorial.config")
+    app.config.from_object("microblog.config")
     app.register_blueprint(frontend.index_bp)
     app.register_blueprint(frontend.login_bp)
     return app
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from tutorial import create_app
+from microblog import create_app
 
-app = create_app("tutorial")
+app = create_app("microblog")
 app.run(debug=True)
